1e1de1a47b2960423941d953cee00d40ce1fc2cd
kailan <Kailan1001101@gmail.com>
2025-05-05 20:38:00 +0200

    Initial files and startup

---
+++ src/main/java/com/kailan/couchesforsale/CouchesforsaleApplication.java
@@ -0,0 +1,13 @@
+package com.kailan.couchesforsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchesforsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchesforsaleApplication.class, args);
+	}
+
+}
